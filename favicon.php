bf361dd506e0411461eb319f4aa662bda55cbf10
Paul Wratt <paul.wratt@gmail.com>
2021-07-13 16:29:36 +1200

    fix: favicon.php

--- favicon.php
+++ favicon.php
@@ -19,12 +19,16 @@ if ( $rf == ''  ) {
 
 $f="x_favicon.ico";
 if (file_exists($f)) {
-    header('Content-Type: image/x-icon');
-    header('Expires: 0');
-    header('Cache-Control: must-revalidate');
-    header('Pragma: public');
-    header('Content-Length: '.filesize($f));
-    readfile($file);
+  header('Content-Type: image/x-icon');
+  header('Expires: 0');
+  header('Cache-Control: must-revalidate');
+  header('Pragma: public');
+  header('Content-Length: '.filesize($f));
+# wrong here file will "hang" output, after headers
+# client side connection cant complete because "Content-Length" bytes never arrive
+# useful to anoy hackers
+# readfile($file);
+  readfile($f);
 }
 
 exit;
